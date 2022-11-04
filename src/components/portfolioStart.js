074537bcd74760babe5f87f16c2eec57b6f5f01e
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-11-04 18:35:20 -0400

    added file structure to components

---
+++ src/components/portfolioStart.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import navbar from './navbar.js';
+import header from './header.js';
+import footer from './footer.js';
+import about from './pages/about.js';
+import contact from './pages/contact.js';
+import portfolio from './pages/portfolio';
+
+export default function porfolio() {
+  const [pageView, setPageView] = useState('portfolio');
+
+const renderPage = () => {
+  if (currentPage === 'portfolio') {
+    return <portfolio/>;
+  }
+  if (currentPage === 'about') {
+    return <about/>;
+  }
+  return <contact/>;
+};
+
+const handlePageChange = (page) => setPageView(page);
+return (
+  <div>
+    <header/>
+    <navbar pageView={pageView} handlePageChange={handlePageChange} />
+    {renderPage()}
+    <footer />
+  </div>
+);
+}
+
