30df04d38c3ae18e75f71d2df2cefa1141227ed9
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-11-07 08:29:49 -0500

    added styling

--- src/components/pages/projects/ProjectCont.js
+++ src/components/pages/projects/ProjectCont.js
@@ -8,8 +8,8 @@ const ProjectCont = ({ project }) => (
     <div className="card-body">
       <h4 className="card-title">{project.name}</h4>
       <p className="card-text">{project.description}</p>
-      <a href={project.repo} className="btn btn-secondary">Github</a>
-      <a href={project.liveLink} className="btn btn-secondary">Live</a>
+      <a href={project.repo} className="btn link-btn">Github</a>
+      <a href={project.liveLink} className="btn link-btn">Live</a>
     </div>
   </div>
 </Container>
