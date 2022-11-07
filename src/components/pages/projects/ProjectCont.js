c7aecedf9eb91f1bb464f6fd31c4a0b8324e818f
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-11-07 08:36:32 -0500

    fixed buttons

--- src/components/pages/projects/ProjectCont.js
+++ src/components/pages/projects/ProjectCont.js
@@ -1,5 +1,9 @@
 import { Container } from "react-bootstrap"
 
+const btn = {
+  color: "#0058b6",
+  backgroundColor: "white", 
+}
 
 const ProjectCont = ({ project }) => (
 <Container>
@@ -8,8 +12,8 @@ const ProjectCont = ({ project }) => (
     <div className="card-body">
       <h4 className="card-title">{project.name}</h4>
       <p className="card-text">{project.description}</p>
-      <a href={project.repo} className="btn link-btn">Github</a>
-      <a href={project.liveLink} className="btn link-btn">Live</a>
+      <a href={project.repo} className="btn me-2" style={btn}>Github</a>
+      <a href={project.liveLink} className="btn me-2" style={btn}>Live</a>
     </div>
   </div>
 </Container>
